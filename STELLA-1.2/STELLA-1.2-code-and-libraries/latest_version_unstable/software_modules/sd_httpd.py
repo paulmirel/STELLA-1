33749d99d759f6e420507f360886f3f82c2dc45f
Alan Grover <awgrover@gmail.com>
2026-05-13 07:09:18 -0400

    sd_httpd.py: catch brokenpipe

--- STELLA-1.2/STELLA-1.2-code-and-libraries/latest_version_unstable/software_modules/sd_httpd.py
+++ STELLA-1.2/STELLA-1.2-code-and-libraries/latest_version_unstable/software_modules/sd_httpd.py
@@ -22,7 +22,7 @@ except ImportError as e:
     print("Can't start http server, no wifi module")
 
 if inet_lan:
-    import os,time,re
+    import time,re
     from software_modules import file_utils
 
     try:
@@ -143,11 +143,10 @@ def _startup(n, wifi_enabled):
         if was_connected:
             HTTPServer.stop()
         was_connected = False
-        print(f"http disconnected (wifi|inet_lan)")
+        print("http disconnected (wifi|inet_lan)")
 
 inet_lan.wifi_module.subscribe( _startup )
 
-import sys
 def update():
     global was_connected
 
@@ -167,6 +166,14 @@ def update():
                     pass
                 else:
                     raise e
+            except adafruit_httpserver.exceptions.InvalidPathError as e:
+                # file-not-exists, backslash, parent-dir
+                print(str(e))
+                # continue
+            except BrokenPipeError:
+                # safe to ignore
+                pass
+
         else:
             url = f"http://{inet_lan.wifi_module.ipv4_address()}:{Port}"
             print(f"HTTP for sd-card starting as {url}")
@@ -176,7 +183,7 @@ def update():
         if was_connected:
             HTTPServer.stop()
             was_connected = False
-            print(f"http disconnected (wifi|inet_lan)")
+            print("http disconnected (wifi|inet_lan)")
 
 if __name__ == "__main__":
     print("Standalone test")
@@ -190,4 +197,3 @@ if __name__ == "__main__":
             update()
     else:
         print("Exit, no inet resource")
-
